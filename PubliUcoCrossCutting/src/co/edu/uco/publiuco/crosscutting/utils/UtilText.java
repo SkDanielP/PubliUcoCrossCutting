dd05c5b3b345ea8a3865ea830b4edd4dad696d97
Daniel <danielprdj@gmail.com>
2023-04-21 22:40:14 -0500

    commit 2

---
+++ PubliUcoCrossCutting/src/co/edu/uco/publiuco/crosscutting/utils/UtilText.java
@@ -0,0 +1,48 @@
+package co.edu.uco.publiuco.crosscutting.utils;
+
+public final class UtilText {
+	
+	private final static UtilText INSTANCE = new UtilText();
+	public static final String EMPTY = "";
+	
+	private UtilText() {
+		super();
+	}
+	
+	public static final UtilText getUtilText() {
+		return INSTANCE;
+	}
+	
+	public final boolean isNull(final String text) {
+		return UtilObject.isNull(text);
+		
+	}
+	
+	public final String getDefault(final String text, final String defaultValue) {
+		
+		String result = text;
+		
+		if (isNull(text)) {
+			result = isNull(defaultValue) ? EMPTY : defaultValue ;
+		}
+		
+		return result;
+	}
+	
+	public final String applyTrim(final String text) {
+		return getDefault(text).trim();
+	}
+	
+	public final boolean matchPattern(final String text, final String pattern) {
+		return getDefault(text).matches(getDefault(pattern));
+	}
+	
+	public final String getDefault(final String text) {
+		return getDefault(text, EMPTY);
+	}
+	
+	public final String getDefaultValue() {
+		return EMPTY;
+	}
+	
+}
